b3d696ca923b4fd02b7899a49563c81e76ecea61
liwei <13193070380@163.com>
2022-02-18 14:38:24 +0800

    1

--- schema_models/base_model.py
+++ schema_models/base_model.py
@@ -4,7 +4,7 @@ from typing import Any
 
 class RespModel(BaseModel):
     status: int = Field(1, description='状态码,1为成功,其余均失败')
-    message: str = Field('成功', description='响应备注,默认成功')
+    message: str = Field('Success', description='响应备注,默认成功')
     data: Any = Field([], description='响应体,返回值为obj')
 
 
