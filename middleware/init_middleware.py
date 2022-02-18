61214976ab10a727ae3eee9ebb8f13a14804aa22
liwei <13193070380@163.com>
2022-02-18 10:32:04 +0800

    增加初始化中间件 挂载db和redis到状态机

--- middleware/init_middleware.py
+++ middleware/init_middleware.py
@@ -1,10 +1,12 @@
 import asyncio
 from fastapi import Request
+from fastapi.responses import JSONResponse
 from starlette.middleware.base import BaseHTTPMiddleware, DispatchFunction, RequestResponseEndpoint
 from starlette.responses import Response
 from starlette.types import ASGIApp
 
 from common.db import get_async_redis_session, get_async_db
+from common.err import HTTPException
 
 
 class InitMiddleware(BaseHTTPMiddleware):
@@ -18,15 +20,14 @@ class InitMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
         # tasks = [get_async_db(), get_async_redis_session()]
 
-        response = {"message": "系统异常", "status": 10000}
         try:
             # noinspection PyTupleAssignmentBalance
             request.state.db = await get_async_db()
             request.state.redis = await get_async_redis_session()
             response = await call_next(request)
-            await request.state.db.close()
-            await request.state.redis.close()
+            # await request.state.db.close()
+            # await request.state.redis.close()
             return response
         except Exception as err:
             print(err)
-            return response
+            return JSONResponse(content={"status": 10000, "message": "系统异常", "data": str(err)})
