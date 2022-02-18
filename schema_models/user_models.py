c5cf3e2005abb43d9a6636f78e59c6b724961a7f
liwei <13193070380@163.com>
2022-02-18 10:31:11 +0800

    整理注册接口传参

--- schema_models/user_models.py
+++ schema_models/user_models.py
@@ -2,6 +2,9 @@ from pydantic import BaseModel, Field, EmailStr
 
 
 class UserRegisterIn(BaseModel):
-    user_name: str = Field(..., description="用户名", min_length=6, max_length=20)
-    password: str = Field(..., description="密码", min_length=8, max_length=20, )
-    email: EmailStr
+    name: str = Field(..., description="用户名", min_length=6, max_length=20)
+    account: str = Field(..., description="登录账号", min_length=6, max_length=20)
+    password: str = Field(..., description="密码", min_length=8, max_length=20)
+    email: EmailStr = Field(..., description="邮箱")
+    code: str = Field('', description="验证码-暂未启用", max_length=6)
+    invite_code: str = Field('', description="邀请码-暂未启用", max_length=6)
