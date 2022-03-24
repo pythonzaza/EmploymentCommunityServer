314d32bb8bc90067a64e10a85e7fcf1c82e2e09c
liwei <13193070380@163.com>
2022-03-24 15:46:39 +0800

    用户模型加注释

--- models/user_models.py
+++ models/user_models.py
@@ -6,11 +6,11 @@ from common.db import Base
 
 class UserModel(Base):
     __tablename__ = "user"
-    id: int = Column(Integer, primary_key=True)
-    name: str = Column(String)
-    account: str = Column(String)
-    password: str = Column(String)
-    email: str = Column(String)
-    token: str = Column(String, server_default="")
-    register_time: DATETIME = Column(DATETIME, server_default=func.now())
-    register_ip: str = Column(String)
+    id: int = Column(Integer, primary_key=True)  # id
+    name: str = Column(String)  # 昵称
+    account: str = Column(String)  # 账号
+    password: str = Column(String)  # 密码
+    email: str = Column(String)  # 邮箱
+    token: str = Column(String, server_default="")  # 根token
+    register_time: DATETIME = Column(DATETIME, server_default=func.now())  # 注册时间
+    register_ip: str = Column(String)  # 注册ip
