df3409f09bd321c050d9b2e15b6123b1b378287f
liwei <13193070380@163.com>
2022-04-01 14:10:24 +0800

    添加gunicorn配置文件

---
+++ gunicorn_config.py
@@ -0,0 +1,15 @@
+import multiprocessing
+
+bind = '0.0.0.0:80'
+workers = multiprocessing.cpu_count() * 2 + 1
+
+backlog = 2048
+worker_class = "gevent"
+worker_connections = 1000
+daemon = True
+debug = True
+proc_name = 'CommunityServer'
+pidfile = './log/gunicorn.pid'
+errorlog = './log/gunicorn.log'
+loglever = "info"
+worker_class = "uvicorn.workers.UvicornWorker"
