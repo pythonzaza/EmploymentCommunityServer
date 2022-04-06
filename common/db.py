e4e0cba5ac49a92ce6f215cd8ac093b33a0c0b00
liwei <13193070380@163.com>
2022-04-06 17:05:20 +0800

    数据库连接改为自动释放

--- common/db.py
+++ common/db.py
@@ -1,21 +1,24 @@
 from aioredis import Redis, from_url
 from sqlalchemy.ext.asyncio import AsyncSession
-# from sqlalchemy.orm import declarative_base
+from collections.abc import AsyncGenerator
 from sqlalchemy.orm import sessionmaker, registry
 
 from configs import async_engine, RedisConfig
 
 mapper_registry = registry()
 Base = mapper_registry.generate_base()
-# Base = declarative_base()
 
 
 async_db_session = sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
 
 
-async def get_async_db() -> AsyncSession:
+# async def get_async_db() -> AsyncSession:
+#     async with async_db_session() as db_session:
+#         return db_session
+
+async def get_async_db() -> AsyncGenerator:
     async with async_db_session() as db_session:
-        return db_session
+        yield db_session
 
 
 async def get_async_redis_session() -> Redis:
