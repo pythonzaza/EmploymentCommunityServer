4233906f9fcb9b091a3dc9ce8fd77a5cd3873212
liwei <13193070380@163.com>
2022-05-18 18:11:34 +0800

    requests  token封装

--- common/request.py
+++ common/request.py
@@ -2,10 +2,15 @@ from aiohttp import ClientSession
 
 
 class Requests(object):
+    METHODS = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS']
+
     def __init__(self, headers=None):
         self.headers = headers if headers else {}
 
     async def request(self, method, url, params=None, json=None, data=None, headers=None, verify_ssl=False, **kwargs):
+        if method not in self.METHODS:
+            raise ValueError(f'Invalid method: {method}')
+
         if headers:
             self.headers.update(headers)
 
@@ -13,6 +18,9 @@ class Requests(object):
             async with session.request(method, url, json=json, data=data, verify_ssl=verify_ssl, params=params,
                                        headers=self.headers, **kwargs) as response:
                 return await response.json()
+
+    async def set_headers(self, headers):
+        self.headers.update(headers)
 
     async def get(self, url, params=None, headers=None, verify_ssl=False, **kwargs):
         return await self.request('GET', url, params=params, headers=headers, verify_ssl=verify_ssl, **kwargs)
